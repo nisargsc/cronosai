c09ae43c38a722d136e30a4c33b1abde0bba26f0
Nisarg <nisarg.chaudhari@webosphere.in>
2025-04-29 15:48:11 +0530

    Add CORSMiddleware to the playground

--- playground/main.py
+++ playground/main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 load_dotenv()
 from agno.playground import Playground, serve_playground_app
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 from agents.cronos import get_cronos_agent
 from agents.tester import get_tester_agent
@@ -16,6 +18,19 @@ app = Playground(
         cronos_agent,
         dev_agent,
 ]).get_app()
+
+# Get allowed origins from environment variable or use default
+UI_URL = os.environ.get("UI_URL", "http://192.168.10.176:3000")
+ALLOWED_ORIGINS = [UI_URL]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,  # Allow UI origin from environment variable
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 if __name__ == "__main__":
